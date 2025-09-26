5f5cecfbdc3d87f0993ecedfe5fe3221cdc03393
MWP-services <micha.honkoop@gmail.com>
2025-09-26 14:37:07 +0200

    Pin react-native-worklets to Expo Go version

--- babel.config.js
+++ babel.config.js
@@ -4,8 +4,10 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'], // of 'module:metro-react-native-babel-preset'
     plugins: [
       // ...andere plugins
-      'react-native-reanimated/plugin',
+      // Worklets plugin must run before Reanimated so that both share the
+      // version pinned in package.json (Expo Go ships Worklets 0.5.1).
       'react-native-worklets/plugin',
+      'react-native-reanimated/plugin',
     ],
   };
 };
